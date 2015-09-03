624f1787804ed48407ddc18e723ec36c1a5594c3
Brijesh Bittu <brijeshb42@gmail.com>
2015-09-03 13:15:42 +0530

    embed fixes

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,5 @@
 var gulp        = require('gulp');
 
 module.exports = function() {
-    gulp.start('html', 'less', 'browserify', 'watch', 'browserify-vendor');
+    gulp.start('html', 'less', 'watch', 'browserify-vendor', 'browserify');
 };
